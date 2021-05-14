959a49f80b247ea9b051696d5956c29d22942dc0
aleksey010 <75483556+aleksey010@users.noreply.github.com>
2021-05-14 21:53:34 +0300

    new111
    new111

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,9 +1,13 @@
 jQuery(document).ready(function(){
-    
+    var dropdown_items = $('.dropdown-item');
     $('.hover-menu').click(function(){
         $('.body-hover-menu').toggleClass('active');
+        $('.body-hover-menu_small').toggleClass('active_small');
+        $('.hover-menu').toggleClass('active_small')
     });
-
+    $('.main-dropdown').click(function(event){
+        $(this).find(".dropdown-item").toggleClass('active');
+    });
     $('#redtext').keyup(function() {
         $('.text-screen').text( $('#redtext').val() );
     });
